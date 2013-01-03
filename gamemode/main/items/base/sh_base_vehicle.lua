7043ea44f233afa0dc7937c6db33734bd17027b9
Chessnut <chessnutist@gmail.com>
2013-01-03 09:44:19 -0800

    Added verification for vehicle scripts.

--- gamemode/main/items/base/sh_base_vehicle.lua
+++ gamemode/main/items/base/sh_base_vehicle.lua
@@ -8,6 +8,20 @@ ITEM.preventDrop = true;
 ITEM.honk = "vu_horn_simple";
 
 function ITEM:OnUse(entity, client)
+	if (self.vehicleScript) then
+		local content = file.Read("scripts/vehicles/"..self.vehicleScript..".txt", "GAME");
+
+		if (!content or content == "") then
+			MsgC(Color(255, 0, 0, 255), "[Pistachio] Invalid vehiclescript for '"..self.uniqueID.."' ("..self.vehiclescript..")\n");
+
+			return;
+		end;
+	else
+		MsgC(Color(255, 0, 0, 255), "[Pistachio] Missing vehiclescript for '"..self.uniqueID.."'\n");
+
+		return;
+	end;
+
 	client:Notify( pistachio:CreateItemVehicle(client, self) );
 
 	return false, false;
