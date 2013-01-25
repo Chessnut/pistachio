a87daad9cd5572db3c24e100f899c11dc4da95f3
Chessnut <chessnutist@gmail.com>
2013-01-25 15:38:22 -0800

    Removed phone hat due to missing model. Changed test hat to cinderblock. Readded tophat and and cinderblock to market.

--- gamemode/main/items/sh_hat_tophat.lua
+++ gamemode/main/items/sh_hat_tophat.lua
@@ -3,10 +3,9 @@ local ITEM = {};
 ITEM.base = "base_hat";
 ITEM.name = "Pistachio Top Hat";
 ITEM.uniqueID = "hat_tophat";
-ITEM.model = "models/pistachio/hats/tophat.mdl";
+ITEM.model = "models/hats/tophat.mdl";
 ITEM.description = "A black colored top hat.";
 ITEM.price = 800;
-ITEM.preventBuy = true;
 ITEM.offset = {1, 4.8, 0};
 ITEM.rotation = {0, -80, 10};
 
